16e2cdc049d94f75bf33094df06855e158c3ae6a
Rukiye <rukiyeeziz@gmail.com>
2020-08-20 21:15:11 -0400

    added sum stuff

--- TestMaven/src/main/java/com/revature/test.java
+++ TestMaven/src/main/java/com/revature/test.java
@@ -4,7 +4,11 @@ public class test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("hell maven");
+		System.out.println("hell maven");
+		int a = 5;
+		int b = 6;
+		int sum = a + b;
+		System.out.println("sum = " + sum);
 	}
 
 }
