ac8c51b16dd65dbf24512f20f2e18442700c39ab
Rukiye Eziz <44105549+RukiyeEziz@users.noreply.github.com>
2020-12-06 18:41:27 -0500

    added springboot project practice crud methods

--- course-api/src/main/java/com/example/controllers/TopicController.java
+++ course-api/src/main/java/com/example/controllers/TopicController.java
@@ -3,13 +3,21 @@ package com.example.controllers;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.models.Topic;
+import com.example.services.TopicService;
 
 @RestController
 public class TopicController {
+	
+	@Autowired
+	private TopicService topicService;
 	
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics() {
@@ -19,13 +27,66 @@ public class TopicController {
 		// {"id":"java","name":"Core Java","description":"Core Java Description"},
 		// {"id":"javascript","name":"JavaScript","description":"JavaScriptDescription"}]
 		//the generated JSON has key names corresponding to properly names of the Topic class
-		return Arrays.asList(
-				new Topic("Spring", "Spring Framework", "Spring Framework Description"),
-				new Topic("java", "Core Java", "Core Java Description"),
-				new Topic("javascript", "JavaScript", "JavaScriptDescription")			
-		);
+		
+//		return Arrays.asList(
+//				new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+//				new Topic("java", "Core Java", "Core Java Description"),
+//				new Topic("javascript", "JavaScript", "JavaScriptDescription")			
+//		);
+		
+		return topicService.getAllTopics();
 	}
 	
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) { 
+		//@PathVariable tell spring that this whatever im expecting is an argument here is actually a variable in a path
+		//http://localhost:8080/topics/java
+		//result: {"id":"java","name":"Core Java","description":"Core Java Description"}
+		return topicService.getTopic(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		//POOSTMAN: 
+		// POST	http://localhost:8080/topics
+		/*
+		 * {
+    			"id": "sql",
+    			"name": "SQL ",
+    			"description": "SQL Description"
+			}
+		 */
+		topicService.addTopic(topic);
+		
+	}
+	@RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+	public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+		//POOSTMAN: 
+		// PUT	http://localhost:8080/topics/sql
+		/*
+		 * {
+    			"id": "postgresql",
+    			"name": "SQL ",
+    			"description": "SQL Description"
+			}
+		 */
+		topicService.updateTopic(topic, id);
+		
+	}
+	@RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+	public void deleteTopic(@RequestBody Topic topic, @PathVariable String id) {
+		//POOSTMAN: 
+		// DELETE	http://localhost:8080/topics/sql
+		/*
+		 * {
+    			"id": "postgresql",
+    			"name": "SQL ",
+    			"description": "SQL Description"
+			}
+		 */
+		topicService.deleteTopic(topic, id);
+		
+	}
 	
 
 }
---
+++ course-api/src/main/java/com/example/services/TopicService.java
@@ -0,0 +1,48 @@
+package com.example.services;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.models.Topic;
+
+@Service
+public class TopicService {
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("Spring", "Spring Framework", "Spring Framework Description"),
+			new Topic("java", "Core Java", "Core Java Description"),
+			new Topic("javascript", "JavaScript", "JavaScriptDescription")));	
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);	
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		for(int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if(t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+		
+	}
+
+	public void deleteTopic(Topic topic, String id) {
+		topics.removeIf(t -> t.getId().equals(id));
+		
+	}
+	
+	
+	
+}
